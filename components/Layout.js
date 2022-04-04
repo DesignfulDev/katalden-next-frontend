7833a0151cca3d1153e705b2a3fd2cc609f73d8b
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-04-04 12:51:26 -0300

    Add Head element

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,23 @@ import Head from 'next/head';
 import Header from './Header';
 import Main from './Main';
 
-export default function Layout() {
+export default function Layout({ title, keywords, description, children }) {
   return (
     <div className="h-screen border-2 border-blue-500">
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+      </Head>
       <Header />
       <Main />
     </div>
   );
 }
+
+Layout.defaultProps = {
+  title: 'Kat Alden | Galeria Virtual',
+  keywords: 'tattoo, abstrato, tribal, futurismo, fotografia, moda sustentável',
+  description:
+    'Galeria virtual dos trabalhos artísticos de Katharine Alden. Tattoos, fotografia, moda sustentável',
+};
