9d9bfcc447f2efd7d1ba6aa19eea17d1a78cb73e
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-05-07 15:20:48 -0300

    Clean up unecessary classes and elements

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { MenuAlt3Icon } from '@heroicons/react/outline';
-import BtnPrimary from './BtnPrimary';
 import GalleryNav from './GalleryNav';
 
 export default function Header() {
   return (
     <header className="fixed w-full z-10 bg-brand-white">
-      <div className="w-full flex justify-between h-20">
-        <div className="mx-3 relative w-14">
-          <Link href="/">
-            <a>
-              <Image
-                src="/images/logo-black.svg"
-                layout="fill"
-                objectFit="contain"
-                alt="Logo"
-              />
-            </a>
-          </Link>
-        </div>
+      <div className="w-full flex justify-between items-center h-20">
+        <Link href="/">
+          <a className="relative mx-3">
+            <Image
+              src="/images/logo-black.svg"
+              height={60}
+              width={60}
+              alt="Logo"
+            />
+          </a>
+        </Link>
 
-        <MenuAlt3Icon className="h-8 self-center px-3" />
+        <MenuAlt3Icon className="h-8 px-3" />
       </div>
 
       <GalleryNav />
