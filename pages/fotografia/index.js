c24fb8cdcaaa647ca5202f74215ed741d30c6f88
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-05-12 22:53:53 -0300

    Create gallery pages

---
+++ pages/fotografia/index.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout';
+import Gallery from '../../components/Gallery';
+
+export default function FotografiaPage() {
+  return (
+    <Layout>
+      <Gallery />
+    </Layout>
+  );
+}
---
+++ pages/roupas/index.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout';
+import Gallery from '../../components/Gallery';
+
+export default function RoupasPage() {
+  return (
+    <Layout>
+      <Gallery />
+    </Layout>
+  );
+}
