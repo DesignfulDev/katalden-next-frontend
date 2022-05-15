32217a77097699a5153ea856517fbbdfc0f9b493
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-05-15 19:41:23 -0300

    Create modal component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,24 +1,10 @@
-import { XIcon } from '@heroicons/react/outline';
+import Modal from './Modal';
 import SidebarNav from './SidebarNav';
-import Copyright from './Copyright';
-import { motion } from 'framer-motion';
 
 export default function Sidebar({ toggle }) {
   return (
-    <motion.div
-      className="fixed left-0 w-full z-20 min-h-screen bg-brand-white flex flex-col justify-between items-center"
-      initial={{ y: '100%' }}
-      animate={{ y: 0 }}
-      exit={{ y: '100%' }}
-      transition={{ duration: 0.2 }}
-    >
-      <button className="w-10 self-end h-20 mx-3" onClick={toggle}>
-        <XIcon />
-      </button>
-
+    <Modal toggle={toggle}>
       <SidebarNav />
-
-      <Copyright />
-    </motion.div>
+    </Modal>
   );
 }
