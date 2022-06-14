dc2835461d2cc430d3439649536a5d63d32d68d9
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-06-13 23:12:18 -0300

    Remove unused imports and variables

--- pages/sobre/index.js
+++ pages/sobre/index.js
@@ -1,19 +1,12 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Modal from '../../components/Modal';
 import SobreContent from './SobreContent';
 import Layout from '../../components/Layout';
 import Copyright from '../../components/Copyright';
 
 export default function SobrePage() {
-  const [showModal, setShowModal] = useState(true);
-
-  const router = useRouter();
-
   return (
     <Layout>
       <div className="flex flex-col items-center bg-brand-white">
-        <h1 className="text-3xl lowercase ">sobre mim</h1>
+        <h1 className="text-3xl lowercase font-thin">sobre mim</h1>
         <SobreContent />
 
         <Copyright />
