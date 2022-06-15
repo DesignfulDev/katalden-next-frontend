c6f17050cf03f14c168bbbcbfd76d9fedcc29a60
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-06-14 21:07:33 -0300

    Create base under-construction page

---
+++ pages/blog/index.js
@@ -0,0 +1,15 @@
+import Layout from '../../components/Layout';
+import Copyright from '../../components/Copyright';
+
+export default function StorePage() {
+  return (
+    <Layout>
+      <div className="flex flex-col items-center justify-between px-8 h-full">
+        <h1 className="text-3xl lowercase">Blog</h1>
+        <p className="text-xl font-light mt-16 mb-10">Em construção</p>
+
+        <Copyright />
+      </div>
+    </Layout>
+  );
+}
