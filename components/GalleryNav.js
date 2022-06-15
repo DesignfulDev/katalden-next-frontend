1da6fd8fc4c3e0cdba9045d988e49e73b0f3c803
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-06-15 14:35:48 -0300

    Refactor gallery navigation

--- components/GalleryNav.js
+++ components/GalleryNav.js
@@ -1,11 +1,21 @@
-import GalleryNavItem from './GalleryNavItem';
+import Link from 'next/link';
+
+const links = [
+  { url: '/tattoo', text: 'tattoo' },
+  { url: '/fotografia', text: 'fotografia' },
+  { url: '/roupas', text: 'roupas' },
+];
 
 export default function GalleryNav() {
   return (
     <nav className="w-full flex justify-around items-center h-8">
-      <GalleryNavItem linkRef="/tattoo" linkText="tattoo" />
-      <GalleryNavItem linkRef="/fotografia" linkText="fotografia" />
-      <GalleryNavItem linkRef="/roupas" linkText="roupas" />
+      {links.map(link => (
+        <Link key={link.url} href={link.url}>
+          <a className="md:px-10 font-light text-sm md:text-xl uppercase tracking-widest">
+            {link.text}
+          </a>
+        </Link>
+      ))}
     </nav>
   );
 }
