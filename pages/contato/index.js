2509a56c0fd3b9a0d16143ba0e756dc98ddb2058
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-06-17 02:27:05 -0300

    Replace select element with HeadlessUI Listbox

--- pages/contato/index.js
+++ pages/contato/index.js
@@ -1,11 +1,24 @@
+import { useState, Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { SelectorIcon, CheckIcon } from '@heroicons/react/outline';
 import Layout from '../../components/Layout';
 import Copyright from '../../components/Copyright';
 
+const options = [
+  { id: 0, option: 'Assunto', value: '' },
+  { id: 1, option: 'Agendar tattoo', value: 'agendamento' },
+  { id: 2, option: 'Informações sobre fotografias', value: 'fotografia' },
+  { id: 3, option: 'Parcerias e colaborações', value: 'colab' },
+  { id: 4, option: 'Outro assunto', value: 'outro' },
+];
+
 export default function ContactPage() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <Layout>
       <div className="flex flex-col items-center px-8 h-full">
-        <h1 className="text-3xl lowercase">Contato</h1>
+        <h1 className="text-3xl lowercase font-thin">Contato</h1>
 
         <form className="flex flex-col items-center w-full h-full justify-between mt-16 pb-10">
           <div className="">
@@ -17,23 +30,80 @@ export default function ContactPage() {
 
             <input
               type="email"
-              placeholder="seu@email.com"
+              placeholder="E-mail"
               className="border-2 border-brand bg-brand-white placeholder:italic placeholder:text-gray-400 w-full py-2 px-4 mb-4 rounded-full"
             />
 
-            <select
+            <Listbox
+              value={selected}
+              onChange={setSelected}
               name="assunto"
-              id="select-assunto"
-              className="border-2 border-brand bg-brand-white rounded-full py-2 px-4 mb-4 w-full"
+              as="div"
             >
-              <option value="" className="py-2">
-                -- Motivo do contato --
-              </option>
-              <option value="agendamento">Agendar Tattoo</option>
-              <option value="fotografia">Informações sobre fotografia</option>
-              <option value="colab">Parcerias e colaborações</option>
-              <option value="outros">Outro assunto</option>
-            </select>
+              <div className="relative mt-1">
+                <Listbox.Button className="relative border-2 border-brand bg-brand-white rounded-full py-2 px-4 mb-4 w-full text-left">
+                  <span
+                    className={`block truncate ${
+                      selected.id === 0 ? 'text-gray-400' : null
+                    }`}
+                  >
+                    {selected.option}
+                  </span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                    <SelectorIcon
+                      className="h-5 w-5 text-brand"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+
+                <Transition
+                  as={Fragment}
+                  enter="transform transition ease-out duration-150"
+                  enterFrom="opacity-0 translate-y-[-3rem]"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transform transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-[-3rem]"
+                >
+                  <Listbox.Options className="absolute -mt-3 ml-5 max-h-60 overflow-auto rounded-md bg-white py-2 px-4 shadow-lg">
+                    {options.slice(1).map(opt => (
+                      <Listbox.Option
+                        key={opt.id}
+                        value={opt}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active
+                              ? 'bg-brand-light text-brand-white'
+                              : 'text-brand-black'
+                          }`
+                        }
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? 'font-medium' : 'font-normal'
+                              }`}
+                            >
+                              {opt.option}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-brand-dark">
+                                <CheckIcon
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
 
             <textarea
               name="mensagem"
