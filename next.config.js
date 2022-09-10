29ca21f9aaf533c7ee082d5664aa10f4513fb93e
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-09-10 00:57:51 -0300

    Add Cloudinary as image loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
+    loader: 'cloudinary',
+    path: 'res.cloudinary.com/designful-dev',
   },
 
   async redirects() {
