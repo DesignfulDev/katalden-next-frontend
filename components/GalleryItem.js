7fc557794c2aa2c50aa840a1806e91b83fff4df2
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-09-09 23:31:33 -0300

    Create GalleryItem component

---
+++ components/GalleryItem.js
@@ -0,0 +1,30 @@
+import Layout from './Layout';
+import ImageSlider from './ImageSlider';
+import Copyright from './Copyright';
+import { Card } from './Card';
+
+export default function GalleryItem({ project, cardFields }) {
+  return (
+    <div>
+      <Layout>
+        <Card>
+          <div>
+            <ImageSlider images={project.attributes.imagens.data} />
+          </div>
+          <section className="overflow-y-scroll text-xl font-thin text-left grow p-7">
+            <Card.Details>
+              {cardFields.map((field, idx) => (
+                <Card.Details.Item key={idx} label={field.label}>
+                  {project.attributes[field.api]}
+                </Card.Details.Item>
+              ))}
+            </Card.Details>
+            <Card.Description>{project.attributes.descricao}</Card.Description>
+
+            <Copyright />
+          </section>
+        </Card>
+      </Layout>
+    </div>
+  );
+}
