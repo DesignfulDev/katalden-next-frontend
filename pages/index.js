9352da91f2c6da6e86ee65868b813dcacad5fc64
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-09-11 20:45:30 -0300

    Redirect entry point to tattoo gallery

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { pathname } = router;
+    if (pathname == '/') {
+      router.push('/tattoos');
+    }
+  }, [router]);
+
   return <Layout></Layout>;
 }
