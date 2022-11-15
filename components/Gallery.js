7eef819ca8d7ff10e5b438a75caabbf5e84a4794
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-11-15 18:10:08 -0300

    Replace Card component with GalleyItem

--- components/Gallery.js
+++ components/Gallery.js
@@ -2,11 +2,8 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
-import Layout from './Layout';
 import Modal from './Modal';
-import ImageSlider from './ImageSlider';
-import Copyright from './Copyright';
-import { Card } from './Card';
+import GalleryItem from '../components/GalleryItem';
 
 export default function Gallery({ projects, cardFields }) {
   const router = useRouter();
@@ -22,71 +19,41 @@ export default function Gallery({ projects, cardFields }) {
 
   return (
     <div>
-      <Layout>
-        {/* MODAL */}
-        {router.query.id && (
-          <Modal isOpen={isOpen} closeModal={closeModal}>
-            <Card>
-              <div>
-                <ImageSlider
-                  images={
-                    projects.filter(
-                      project => project.id === +router.query.id
-                    )[0].attributes.imagens.data
-                  }
-                />
-              </div>
-              <section className="overflow-y-scroll text-xl font-thin text-left grow p-7">
-                <Card.Details>
-                  {cardFields.map((field, idx) => (
-                    <Card.Details.Item key={idx} label={field.label}>
-                      {
-                        projects.filter(
-                          project => project.id === +router.query.id
-                        )[0].attributes[field.api]
-                      }
-                    </Card.Details.Item>
-                  ))}
-                </Card.Details>
-                <Card.Description>
-                  {
-                    projects.filter(
-                      project => project.id === +router.query.id
-                    )[0].attributes.descricao
-                  }
-                </Card.Description>
+      {/* MODAL */}
+      {router.query.id && (
+        <Modal isOpen={isOpen} closeModal={closeModal}>
+          <GalleryItem
+            project={
+              projects.filter(project => project.id === +router.query.id)[0]
+            }
+            cardFields={cardFields}
+          />
+        </Modal>
+      )}
 
-                <Copyright />
-              </section>
-            </Card>
-          </Modal>
-        )}
+      {/* GRID GALLERY */}
+      <section className="grid gap-0.5 grid-cols-3 auto-rows-auto w-full overflow-y-scroll">
+        {projects.length === 0 && <h3>Sem projetos, por enquanto</h3>}
 
-        {/* GRID GALLERY */}
-        <section className="grid gap-0.5 grid-cols-3 auto-rows-auto w-full overflow-y-scroll">
-          {projects.length === 0 && <h3>Sem projetos, por enquanto</h3>}
-
-          {projects.map(project => (
-            <Link
-              key={project.id}
-              href={`/${router.pathname.slice(1)}?id=${project.id}`}
-              as={`/${router.pathname.slice(1)}/${project.id}`}
-            >
-              <a className="relative w-full aspect-square">
-                <Image
-                  src={project.attributes.imagens.data[0].attributes.hash}
-                  alt={
-                    project.attributes.imagens.data[0].attributes
-                      .alternativeText
-                  }
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </a>
-            </Link>
-          ))}
-        </section>
-      </Layout>
+        {projects.map(project => (
+          <Link
+            key={project.id}
+            href={`/${router.pathname.slice(1)}?id=${project.id}`}
+            as={`/${router.pathname.slice(1)}/${project.id}`}
+          >
+            <a className="relative w-full aspect-square">
+              <Image
+                src={project.attributes.imagens.data[0].attributes.hash}
+                alt={
+                  project.attributes.imagens.data[0].attributes.alternativeText
+                }
+                layout="fill"
+                objectFit="cover"
+              />
+            </a>
+          </Link>
+        ))}
+      </section>
     </div>
   );
 }
