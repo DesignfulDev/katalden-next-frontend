f1a2ec5289b6e11618a8c7059db2987ad4a11f0d
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-12-28 18:56:24 -0300

    Adds funcionality to submit the Contact form and display a user friendly message with feedback of the action

--- pages/contato/index.js
+++ pages/contato/index.js
@@ -1,8 +1,10 @@
 import { useState, Fragment } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { SelectorIcon, CheckIcon } from '@heroicons/react/outline';
+import toast, { Toaster } from 'react-hot-toast';
 import Layout from '../../components/Layout';
 import Copyright from '../../components/Copyright';
+import { API_URL } from '../../config/index';
 
 const options = [
   { id: 0, option: 'Assunto', value: '' },
@@ -13,41 +15,108 @@ const options = [
 ];
 
 export default function ContactPage() {
-  const [selected, setSelected] = useState(options[0]);
+  const [formValues, setFormValues] = useState({
+    nome: '',
+    email: '',
+    mensagem: '',
+    assunto: options[0],
+  });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    // Validation
+    const hasEmptyFields =
+      Object.values(formValues).some(el => el === '') ||
+      formValues.assunto.value === '';
+
+    if (hasEmptyFields) {
+      toast.error('Favor preencher todos os campos');
+      return;
+    }
+
+    const formData = {
+      data: { ...formValues, assunto: formValues.assunto.value },
+    };
+
+    const res = await fetch(`${API_URL}/api/mensagens`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!res.ok) {
+      toast.error('Ops! Algo deu errado. Tente de novo daqui a pouco.');
+    } else {
+      toast.success('Sua mensagem foi enviada para a Kat!');
+      setFormValues({ nome: '', email: '', mensagem: '', assunto: options[0] });
+    }
+  };
+
+  const handleInputChange = e => {
+    if (e.target) {
+      const { name, value } = e.target;
+      setFormValues({ ...formValues, [name]: value });
+    } else {
+      setFormValues({ ...formValues, assunto: { ...e } });
+    }
+  };
 
   return (
     <Layout>
-      <div className="flex flex-col items-center px-8 h-full">
-        <h1 className="text-3xl lowercase font-thin">Contato</h1>
+      <Toaster
+        toastOptions={{
+          duration: 2000,
+          style: {
+            borderRadius: '100px',
+          },
+        }}
+      />
+      <div className="flex flex-col items-center h-full px-8">
+        <h1 className="text-3xl font-thin lowercase">Contato</h1>
 
-        <form className="flex flex-col items-center w-full h-full justify-between mt-16 pb-10">
+        <form
+          className="flex flex-col items-center justify-between w-full h-full pb-10 mt-16"
+          onSubmit={handleSubmit}
+        >
           <div className="">
             <input
               type="text"
+              name="nome"
+              id="nome"
               placeholder="Nome"
-              className="border-2 border-brand bg-brand-white placeholder:italic placeholder:text-gray-400 w-full py-2 px-4 mb-4 rounded-full"
+              onChange={handleInputChange}
+              value={formValues.nome}
             />
 
             <input
               type="email"
+              name="email"
+              id="email"
               placeholder="E-mail"
-              className="border-2 border-brand bg-brand-white placeholder:italic placeholder:text-gray-400 w-full py-2 px-4 mb-4 rounded-full"
+              onChange={handleInputChange}
+              value={formValues.email}
             />
 
             <Listbox
-              value={selected}
-              onChange={setSelected}
+              value={formValues.assunto}
+              onChange={handleInputChange}
               name="assunto"
+              id="assunto"
               as="div"
             >
               <div className="relative mt-1">
                 <Listbox.Button className="relative border-2 border-brand bg-brand-white rounded-full py-2 px-4 mb-4 w-full text-left">
                   <span
                     className={`block truncate ${
-                      selected.id === 0 ? 'text-gray-400 italic' : null
+                      formValues.assunto.id === 0
+                        ? 'text-gray-400 italic'
+                        : null
                     }`}
                   >
-                    {selected.option}
+                    {formValues.assunto.option}
                   </span>
                   <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
                     <SelectorIcon
@@ -107,15 +176,19 @@ export default function ContactPage() {
 
             <textarea
               name="mensagem"
-              id=""
+              id="mensagem"
+              onChange={handleInputChange}
               cols="25"
               rows="6"
               placeholder="Mensagem"
-              className="rounded-3xl border-2 border-brand bg-brand-white placeholder:italic w-full p-4"
+              value={formValues.mensagem}
             ></textarea>
           </div>
 
-          <button className="px-16 py-3  rounded-full bg-brand hover:bg-brand-light active:bg-brand-dark text-brand-white text-lg lowercase">
+          <button
+            className="px-16 py-3 text-lg lowercase rounded-full bg-brand hover:bg-brand-light active:bg-brand-dark text-brand-white"
+            type="submit"
+          >
             enviar
           </button>
         </form>
