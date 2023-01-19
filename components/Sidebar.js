5f52f117a89425f8d5a9145cc140e551110af1e0
Mauricio Lacerda <mauricio.lacerda@pm.me>
2023-01-18 23:30:06 -0300

    Refactor sidebar to also work on screens larger than mobile

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,9 +17,9 @@ export default function Sidebar({ onClose }) {
   };
 
   return (
-    <div className="flex flex-col justify-between h-screen">
-      <nav className="flex items-center justify-center w-full grow">
-        <ul className="text-3xl font-light tracking-wide text-center lowercase">
+    <div className="flex flex-col justify-between h-screen md:h-full">
+      <nav className="flex items-center justify-center w-full grow md:justify-start md:ml-10 md:items-start">
+        <ul className="text-3xl font-light tracking-wide text-center lowercase md:text-xl md:text-left md:pl-10 md:font-normal">
           {links.map(link => (
             <li key={link.url} onClick={handleClose} className="py-4">
               <Link href={link.url} scroll={false}>
