6413e22052178576620b9b94b4b3e4c1705e1d4d
Mauricio Lacerda <mauricio.lacerda@pm.me>
2023-01-18 23:21:26 -0300

    Add variable classes prop to customize the button at each implementation of the component

--- components/BtnPrimary.js
+++ components/BtnPrimary.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 
-export default function BtnPrimary({ btnText, linkTo }) {
+export default function BtnPrimary({ btnText, linkTo, classes }) {
   return (
     <Link href={linkTo}>
-      <a className="self-center py-3 my-4 text-lg lowercase rounded-full md:flex px-7 bg-brand hover:bg-brand-light active:bg-brand-dark text-brand-white">
+      <a
+        className={`self-center py-3 my-4 text-lg lowercase rounded-full px-7 bg-brand hover:bg-brand-light active:bg-brand-dark text-brand-white w-max justify-self-center ${classes}`}
+      >
         {btnText}
       </a>
     </Link>
