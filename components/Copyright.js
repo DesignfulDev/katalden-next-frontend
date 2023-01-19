bc9a4c093314631817e840a1d6a5c35701a26010
Mauricio Lacerda <mauricio.lacerda@pm.me>
2023-01-18 23:22:23 -0300

    Add class rule for better formatting on larger devices

--- components/Copyright.js
+++ components/Copyright.js
@@ -1,6 +1,6 @@
 export default function Copyright() {
   return (
-    <footer className="py-5 mx-auto text-sm font-light text-center">
+    <footer className="py-5 mx-auto text-sm font-light text-center md:text-[1vw]">
       <p className="uppercase">
         Copyright &copy; {new Date().getFullYear()} Katharine Alden
       </p>
